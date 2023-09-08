bacd10e3de79c273f717c43c667559f5789adbf9
김지영 <kimjiyeong@gimjiyeong-ui-Macmini.local>
2023-09-08 18:08:43 +0900

    design : fe css 완료

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Index from "./pages/index";
 import NotFound from "./pages/NotFound";
+import Chat from './pages/Chat';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Index />}></Route>
+          <Route path='/chat' element={<Chat/>}></Route>
           <Route path='*' element={<NotFound />}></Route>
         </Routes>
       </BrowserRouter>
--- fe/src/pages/index.jsx
+++ fe/src/pages/index.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
 
 const Index = () => {
+    const [nickname, setNickname] = useState('')
+    const onchange = (event) => {
+        event.preventDefault()
+        setNickname(event.target.value)
+    }
+
   return (
-    <div className="text-3xl font-bold text-sky-400">hello world!</div>
+    <div className='rounded w-max min-w-fit mx-auto my-16 shadow-2xl'>
+        <div className='flex justify-center items-center p-2'>
+            <h2 className='font-bold text-2xl text-green-600'>Welcome 대나무숲</h2>
+        </div>
+        <div className='p-3'>
+            <form className='flex gap-3 justify-center items-center'>
+                <div>닉네임</div>
+                <input className='border rounded p-3' type='text' value={nickname} onChange={onchange} required/>
+            </form>
+            
+                <Link to={`/chat?username=${encodeURIComponent(nickname)}`}>
+                    <div className='flex justify-center items-center bg-green-200 rounded p-3 mt-12 font-bold hover:bg-green-300 cursor-pointer'>
+                        입장
+                    </div>
+                </Link> 
+            
+        </div>
+    </div>
   )
 }
 
